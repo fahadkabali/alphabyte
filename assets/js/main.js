d72f9bc51296e416b312d5fe2dbe771f0dc61c2c
fahadkabali <kabalifahad@gmail.com>
2025-03-29 18:35:29 +0300

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,3 @@
-/**
-* Template Name: Green
-* Template URL: https://bootstrapmade.com/green-free-one-page-bootstrap-template/
-* Updated: Aug 07 2024 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-
 (function() {
   "use strict";
 
